cd8d95bcea812f2879f92566fac8b07d41fe91a8
Default <jordanhui19@hotmail.com>
2025-10-13 02:54:36 -0400

    added code to break items list into sections in React Products.tsx, can be reused for search probably

--- React/src/pages/Products.tsx
+++ React/src/pages/Products.tsx
@@ -17,10 +17,61 @@ function Products() {
             .catch((err) => console.error(`Could not fetch games for ${con}: `, err))
     }, [brand, con]);
 
+    /*let accessories: any[] = [];
+    let consoles: any[] = [];
+    let controllers: any[] = [];
+    let games: any[] = [];
+    let others: any[] = [];*/
+    
+    const productGroups = {
+        Accessories: [],
+        Consoles: [],
+        Controllers: [],
+        Games: [],
+        Others: []
+    };
+
+    const sectionOrder = ['Consoles', 'Controllers', 'Accessories', 'Games', 'Others']
+
+    products.forEach( p => {
+        console.log(p['productType']);
+        switch ((p['productType'] as String).toLowerCase()) {
+            case 'accessory':
+                productGroups['Accessories'].push(p);
+                break;
+            case 'console':
+                productGroups['Consoles'].push(p);
+                break;
+            case 'controller':
+                productGroups['Controllers'].push(p);
+                break;
+            case 'game':
+                productGroups['Games'].push(p);
+                break;
+            default:
+                productGroups['Others'].push(p);
+                break;
+        }
+    })
+
     return (
         <div>
-            <h1>Games</h1>
-            {products.map((product) => (<Product product={product}/>))}
+            <h1>{con}</h1>
+            {
+                sectionOrder.map(section => {
+                    const items = (productGroups as any)[section];
+                    if (items.length < 1) {
+                        return null;
+                    }
+
+                    return (
+                        <div>
+                            <h2>{section}</h2>
+                            {items.map((i: Product) => (<Product product={i}/>))}
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
