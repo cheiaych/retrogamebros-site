10e57606ef07183e1ef7f8e1ef7bd7056affdfb5
Default <jordanhui19@hotmail.com>
2026-01-13 07:02:18 -0500

    temp renames

--- express/auth.js
+++
@@ -1,6 +0,0 @@
-export function isAdmin (req, res, next) {
-    if (!req.session?.isAdmin) {
-        return res.status(403).json({ error: 'Forbidden' })
-    }
-    next();
-}
--- express/scripts/password-gen.js
+++
@@ -1,5 +0,0 @@
-import bcrypt from 'bcrypt';
-
-const plainPassword = 'password';
-const hash = await bcrypt.hash(plainPassword, 10);
-console.log(hash);
